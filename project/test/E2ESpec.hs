880b6df6fff2ec4812b212ad86cb97e5be4bdb5e
Wiktoria Wróbel <01180686@pw.edu.pl>
2026-06-25 23:03:14 +0200

    example sheets

--- project/test/E2ESpec.hs
+++ project/test/E2ESpec.hs
@@ -102,3 +102,64 @@ spec = describe "End-to-End Correctness" $ do
         Map.lookup ("B", 4) env `shouldBe` Just (NumV 2.5)
         Map.lookup ("B", 5) env `shouldBe` Just (NumV 25.0)
         Map.lookup ("B", 6) env `shouldBe` Just (NumV 5.0)
+
+  it "evaluates the errors.sheet example file correctly" $ do
+    sheetStr <- readFile "spreadsheets/errors.sheet"
+    case parseSheet sheetStr of
+      Left err -> expectationFailure err
+      Right sheet -> do
+        let env = evalSheet sheet
+        Map.lookup ("A", 1) env `shouldBe` Just (ErrV "#DIV/0!")
+        Map.lookup ("A", 2) env `shouldBe` Just (ErrV "#VALUE!")
+        Map.lookup ("A", 3) env `shouldBe` Just (ErrV "#VALUE!")
+        Map.lookup ("A", 4) env `shouldBe` Just (ErrV "Missing reference: (\"Z\",99)")
+        Map.lookup ("A", 5) env `shouldBe` Just (ErrV "#DIV/0!")
+        Map.lookup ("A", 6) env `shouldBe` Just (ErrV "#VALUE!")
+        Map.lookup ("A", 7) env `shouldBe` Just (ErrV "Missing reference: (\"Z\",99)")
+        Map.lookup ("B", 1) env `shouldBe` Just (NumV 100.0)
+        Map.lookup ("B", 2) env `shouldBe` Just (NumV 200.0)
+        Map.lookup ("B", 3) env `shouldBe` Just (ErrV "#DIV/0!")
+
+  it "evaluates the datatypes.sheet example file correctly" $ do
+    sheetStr <- readFile "spreadsheets/datatypes.sheet"
+    case parseSheet sheetStr of
+      Left err -> expectationFailure err
+      Right sheet -> do
+        let env = evalSheet sheet
+        Map.lookup ("A", 1) env `shouldBe` Just (NumV 3.14159)
+        Map.lookup ("A", 2) env `shouldBe` Just (NumV (-42.0))
+        Map.lookup ("B", 1) env `shouldBe` Just (BoolV True)
+        Map.lookup ("B", 2) env `shouldBe` Just (BoolV False)
+        Map.lookup ("C", 1) env `shouldBe` Just (StrV "hello")
+        Map.lookup ("C", 2) env `shouldBe` Just (StrV "world")
+        Map.lookup ("C", 3) env `shouldBe` Just (StrV "hello")
+        Map.lookup ("D", 1) env `shouldBe` Just (BoolV True)
+        Map.lookup ("D", 2) env `shouldBe` Just (BoolV False)
+        Map.lookup ("D", 3) env `shouldBe` Just (BoolV True)
+        Map.lookup ("D", 4) env `shouldBe` Just (BoolV False)
+        Map.lookup ("D", 5) env `shouldBe` Just (BoolV True)
+        Map.lookup ("E", 1) env `shouldBe` Just (BoolV False)
+        Map.lookup ("E", 2) env `shouldBe` Just (BoolV False)
+        Map.lookup ("E", 3) env `shouldBe` Just (ErrV "#VALUE!")
+        Map.lookup ("E", 4) env `shouldBe` Just (ErrV "#VALUE!")
+
+  it "evaluates the complex_deps.sheet example file correctly" $ do
+    sheetStr <- readFile "spreadsheets/complex_deps.sheet"
+    case parseSheet sheetStr of
+      Left err -> expectationFailure err
+      Right sheet -> do
+        let env = evalSheet sheet
+        Map.lookup ("A", 1) env `shouldBe` Just (NumV 10.0)
+        Map.lookup ("A", 2) env `shouldBe` Just (NumV 20.0)
+        Map.lookup ("A", 3) env `shouldBe` Just (NumV 30.0)
+        Map.lookup ("A", 4) env `shouldBe` Just (NumV 40.0)
+        Map.lookup ("B", 1) env `shouldBe` Just (NumV 30.0)
+        Map.lookup ("B", 2) env `shouldBe` Just (NumV 50.0)
+        Map.lookup ("B", 3) env `shouldBe` Just (NumV 70.0)
+        Map.lookup ("C", 1) env `shouldBe` Just (NumV 150.0)
+        Map.lookup ("C", 2) env `shouldBe` Just (NumV 40.0)
+        Map.lookup ("D", 1) env `shouldBe` Just (NumV 300.0)
+        Map.lookup ("D", 2) env `shouldBe` Just (NumV 260.0)
+        Map.lookup ("E", 1) env `shouldBe` Just (NumV 10.0)
+        Map.lookup ("E", 2) env `shouldBe` Just (NumV 300.0)
+        Map.lookup ("E", 3) env `shouldBe` Just (NumV 8.0)
